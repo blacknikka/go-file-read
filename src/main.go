3dba4428e85fcbeaa6b767436c92fcada2308f8b
Yasuhiro <yasuhiro1026a@hotmail.com>
2019-11-04 11:56:53 +0900

    Walk across the indicated directory by command argment.
    e.g.
    	a.out /path/to/directory/

--- src/main.go
+++ src/main.go
@@ -2,11 +2,21 @@ package main
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/blacknikka/go-file-read/src/read"
 )
 
 func main() {
-	fmt.Println("Hello, World")
-	read.Read()
+	if len(os.Args) != 2 {
+		fmt.Println("You should indicate a directory path!")
+		fmt.Println("	For instance,")
+		fmt.Println()
+		fmt.Println("		app.exe /path/to/directory")
+		fmt.Println()
+		os.Exit(1)
+	}
+
+	// ディレクトリのパスを指定して実行
+	read.Read(os.Args[1])
 }
--- src/read/read_file.go
+++ src/read/read_file.go
@@ -2,21 +2,35 @@ package read
 
 import (
 	"fmt"
-	"io/ioutil"
 	"os"
+	"path/filepath"
 )
 
 // Read ファイルを読み込む
-func Read() {
+func Read(dirPath string) {
 	fmt.Println("Start reading a file.")
+	fmt.Println("Target directory is " + dirPath)
+
 	// ファイルをOpenする
-	f, err := os.Open("test.txt")
+	err := filepath.Walk(dirPath, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			fmt.Printf("prevent panic by handling failure accessing a path %q: %v\n", path, err)
+			return err
+		}
+		if info.IsDir() == false {
+			fmt.Println("A file is found : " + path)
+			return nil
+		}
+
+		return nil
+	})
 
 	if err != nil {
 		fmt.Println("error")
+		return
 	}
-	defer f.Close()
+	// defer f.Close()
 
-	b, err := ioutil.ReadAll(f)
-	fmt.Println(string(b))
+	// b, err := ioutil.ReadAll(f)
+	// fmt.Println(string(b))
 }
